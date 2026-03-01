0ae4e71ff57e92eb963222cd83786e396764d88c
Seraphin Eyala <eyalas472@gmail.com>
2026-03-01 23:03:56 +0100

    Ajout tests phase1

--- phase1/tests.js
+++ phase1/tests.js
@@ -1,13 +1,14 @@
 console.log("Début des tests ");
 
-// ------------------cette partie c'est pour la logique mathématique ---------------------------------------------------------
+//cette partie c'est pour la logique mathématique ---------------------------------------------------------
 
 // Test 1 : Distance entre 2 points 
+
 //Arrange
-let ax = 10, ay = 10; // point A
-let bx = 10, by = 20; // point B
+let ax = 10, ay = 10;  
+let bx = 10, by = 20; 
 //Act
-let distance_1 = calculer_distance_entre_deux_points(ax, ay, bx, by); // on appelle la fonction
+let distance_1 = calculer_distance_entre_deux_points(ax, ay, bx, by); 
 //Assert
 if (distance_1 === 10) {
     console.log("Test 1 : Succès -> Résultat : 10");
@@ -16,10 +17,11 @@ if (distance_1 === 10) {
 }
 
 // Test 2 : Distance entre le même point 
+
 //Arrange
-let cx = 5, cy = 5; // même point des deux côtés
+let cx = 5, cy = 5; 
 //Act
-let distance_2 = calculer_distance_entre_deux_points(cx, cy, cx, cy); // distance avec lui-même
+let distance_2 = calculer_distance_entre_deux_points(cx, cy, cx, cy); 
 //Assert
 if (distance_2 === 0) {
     console.log("Test 2 : Succès -> Résultat : 0");
@@ -27,12 +29,13 @@ if (distance_2 === 0) {
     console.error("Test 2 : Échec -> Attendu : 0, Reçu : " + distance_2);
 }
 
-// cette partie c'est pour la partie représentative de canevas ---------------------------------------------------------
+//cette partie c'est pour la partie représentative de canevas ---------------------------------------------------------
 
 // Test 3 : Deux appels doivent donner deux couleurs différentes
+
 //Arrange
-let couleur_1 = genererCouleurAleatoire(); // premiere couleur
-let couleur_2 = genererCouleurAleatoire(); // deuxieme couleur
+let couleur_1 = genererCouleurAleatoire(); 
+let couleur_2 = genererCouleurAleatoire(); 
 //Act + Assert
 if (couleur_1 !== couleur_2) {
     console.log("Test 3 : Succès -> Les deux couleurs sont différentes");
@@ -41,10 +44,11 @@ if (couleur_1 !== couleur_2) {
 }
 
 // Test 4 : pas de couleur dans la liste 
+
 //Arrange
-listeDesPoints = []; // on part d'une liste vide
+listeDesPoints = []; // le liste qui est vide
 //Act
-let res = couleurDejaUtilisee("rgb(100,100,100)"); // couleur qui n'est pas dans la liste
+let res = couleurDejaUtilisee("rgb(100,100,100)"); // couleur qui n'est pas dans liste
 //Assert
 if (res === false) {
     console.log("Test 4 : Succès -> Couleur absente détectée correctement");
@@ -53,6 +57,7 @@ if (res === false) {
 }
 
 // Test 5 : Couleur présente dans la liste 
+
 //Arrange
 listeDesPoints = [{ posX: 10, posY: 10, couleur: "rgb(200,100,50)" }]; // on met un point dans la liste
 //Act
